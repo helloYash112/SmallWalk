5201bbbcc1cd64741f074846ed89c99cbd67309d
yash Wardhan <159517620+helloYash112@users.noreply.github.com>
2024-12-29 17:24:59 +0000

    updated data base

--- src/components/disc.js
+++ src/components/disc.js
@@ -1,58 +1,83 @@
-export const disc=[
-    {
-        name:'Beverages',
-        img:"https://cdn.shopify.com/s/files/1/0516/3509/3665/products/AdobeStock_171926060.jpg?v=1608172997",
-        desc:" Any of various liquids for drinking: The menu lists several beverages, including water, soda, tea, and coffee."
-    },
-    {
-        name:'Lettuce',
-        img:'https://cdn.britannica.com/77/170677-050-F7333D51/lettuce.jpg',
-        desc:'Lettuce (Lactuca sativa) is an annual plant of the family Asteraceae mostly grown as a leaf vegetable. The leaves are most often used raw in green salads, although lettuce is also seen in other kinds of food, such as sandwiches, wraps and soups'
-    },
-    {
-        name:'Fries',
-        img:'https://www.chewoutloud.com/wp-content/uploads/2023/01/Sweet-Potato-Fries-on-White-Plate-with-Ketchup.jpg',
-        desc:'Fries refer to long thin pieces of potato that are deep-fried and usually salted, typically served as a snack or side dish'
-    },
-    {
-        name:'Teetotaller',
-        img:'https://cdn2.vectorstock.com/i/1000x1000/78/86/warning-signs-alcohol-vector-2707886.jpg',
-        desc:'a person who never drinks or consumed alcohol'
-    },
-    {
-        name:'Mango',
-        img:'https://img.freepik.com/premium-photo/ripe-mango-fruits-with-slices-isolated-white_80510-681.jpg?w=2000',
-        desc:'mango is a fruit..'
-    },
+export const disc = [
+  {
+    name: "Beverages",
+    img: "https://cdn.shopify.com/s/files/1/0516/3509/3665/products/AdobeStock_171926060.jpg?v=1608172997",
+    desc: " Any of various liquids for drinking: The menu lists several beverages, including water, soda, tea, and coffee.",
+  },
+  {
+    name: "Lettuce",
+    img: "https://cdn.britannica.com/77/170677-050-F7333D51/lettuce.jpg",
+    desc: "Lettuce (Lactuca sativa) is an annual plant of the family Asteraceae mostly grown as a leaf vegetable. The leaves are most often used raw in green salads, although lettuce is also seen in other kinds of food, such as sandwiches, wraps and soups",
+  },
+  {
+    name: "Fries",
+    img: "https://www.chewoutloud.com/wp-content/uploads/2023/01/Sweet-Potato-Fries-on-White-Plate-with-Ketchup.jpg",
+    desc: "Fries refer to long thin pieces of potato that are deep-fried and usually salted, typically served as a snack or side dish",
+  },
+  {
+    name: "Teetotaller",
+    img: "https://cdn2.vectorstock.com/i/1000x1000/78/86/warning-signs-alcohol-vector-2707886.jpg",
+    desc: "a person who never drinks or consumed alcohol",
+  },
+  {
+    name: "Mango",
+    img: "https://img.freepik.com/premium-photo/ripe-mango-fruits-with-slices-isolated-white_80510-681.jpg?w=2000",
+    desc: "mango is a fruit..",
+  },
 
-    {
-        name:'Stawberry',
-        img:'https://cdn.britannica.com/22/75922-050-D3982BD0/flowers-fruits-garden-strawberry-plant-species.jpg',
-        desc:'Stawberry is a red in color it is a very sweet in test'
-    },
-    {
-        name :'grapes',
-        img :'https://www.almanac.com/sites/default/files/image_nodes/green-grapes.jpg',
-        desc :'green colour sweet taste '
-
-    },{
-        name:'Cricket',
-        img:'https://cdn.britannica.com/63/211663-050-A674D74C/Jonny-Bairstow-batting-semifinal-match-England-Australia-2019.jpg',
-        desc:'criket is a one of the popular game',
-    },
-    {
-        name:'Food Scarcity',
-        img:'https://foodbunker.co.uk/cdn/shop/articles/food_and_starvation_1024x1024.png?v=1677762738',
-        desc:'Food scarcity, also known as food insecurity, is the lack of access and availability of food with a certain group of individuals',
-    },{
-        name:'Stuned',
-        img:'https://cdn.langeek.co/photo/20195/original/amazed?type=jpeg',
-        desc:'so shocked that one is temporarily unable to react; astonished'
-    },
-    {
-        name:'Terrifying',
-        img:'http://www.photo-dictionary.com/photofiles/list/9733/13212terrified.jpg',
-        desc:'causing extreme fear'
-    }
-
-    ];
+  {
+    name: "Stawberry",
+    img: "https://cdn.britannica.com/22/75922-050-D3982BD0/flowers-fruits-garden-strawberry-plant-species.jpg",
+    desc: "Stawberry is a red in color it is a very sweet in test",
+  },
+  {
+    name: "grapes",
+    img: "https://www.almanac.com/sites/default/files/image_nodes/green-grapes.jpg",
+    desc: "green colour sweet taste ",
+  },
+  {
+    name: "Cricket",
+    img: "https://cdn.britannica.com/63/211663-050-A674D74C/Jonny-Bairstow-batting-semifinal-match-England-Australia-2019.jpg",
+    desc: "criket is a one of the popular game",
+  },
+  {
+    name: "Food Scarcity",
+    img: "https://foodbunker.co.uk/cdn/shop/articles/food_and_starvation_1024x1024.png?v=1677762738",
+    desc: "Food scarcity, also known as food insecurity, is the lack of access and availability of food with a certain group of individuals",
+  },
+  {
+    name: "Stuned",
+    img: "https://cdn.langeek.co/photo/20195/original/amazed?type=jpeg",
+    desc: "so shocked that one is temporarily unable to react; astonished",
+  },
+  {
+    name: "Terrifying",
+    img: "https://1387905758.rsc.cdn77.org/data/resource_icons/38/38597.jpg?1694044548",
+    desc: "causing extreme fear",
+  },
+  {
+    name: "Hurry Up",
+    img: "https://thumbs.dreamstime.com/z/photo-athlete-runners-running-track-portrait-athlete-runners-running-track-210188926.jpg",
+    desc: "our flight is soon,hurry up!",
+  },
+  {
+    name: "To Hang Out",
+    img: "https://wallpaperaccess.com/full/136008.jpg",
+    desc: "travel or visit to some places",
+  },
+  {
+    name: "Slopy",
+    img: "https://thumbs.dreamstime.com/b/hiking-trail-overlooking-homes-mountain-sky-curving-rugged-slopy-terrain-moab-utah-beyond-scenic-view-towering-129135027.jpg",
+    desc: "smooth surface or light water surface",
+  },
+  {
+    name: "Starvation",
+    img: "https://seo-manager.s3.amazonaws.com/prod/public/seo/blog-images/blog/how-long-can-you-survive-without-food/257a04b9-3fc1-4ca5-8cd0-3e7fefce9ead",
+    desc: "suffering or death caused by lack of food",
+  },
+  {
+    name: "Reincarneted",
+    img: "https://timstaples.com/2019/wp-content/uploads/2015/09/ReincarnationII-2.jpg",
+    desc: "a man may be reincarnated in animal form",
+  },
+];
